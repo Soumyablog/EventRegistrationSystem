ecda6a34d57332f8ba2052de8917c244b7c0eda0
soumyareddymaram <soumyareddymaram@gmail.com>
2026-09-25 16:34:57 +0800

    Changed progam file events to home

--- Program.cs
+++ Program.cs
@@ -73,7 +73,7 @@ public partial class Program
 
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Event}/{action=Index}/{id?}");
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
         // ----------------------------------------------------
         // Seed admin account
